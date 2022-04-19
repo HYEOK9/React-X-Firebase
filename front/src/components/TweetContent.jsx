495256d72c1c30f8482fc7d5b69c96fe45d101e5
HYEOK9 <leejaehyuck9@gmail.com>
2022-04-19 23:29:59 +0900

    feat:editTweet

--- front/src/components/TweetContent.jsx
+++ front/src/components/TweetContent.jsx
@@ -1,21 +1,50 @@
 import { doc, deleteDoc } from 'firebase/firestore';
-
+import { useState } from 'react';
 const TweetContent = ({ tweet, isOwner, db }) => {
+    const [edit, setEdit] = useState(false);
+    const [newContent, setNewContent] = useState(tweet.content);
     const onDelete = async (id) => {
         if (window.confirm('삭제 하시겠습니까?')) {
             await deleteDoc(doc(db, 'tweets', id));
         }
     };
-    const onEdit = async (id) => {
-        console.log('기능 구현중');
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        tweet.content = newContent;
+    };
+    const onChange = (event) => {
+        const {
+            target: { value },
+        } = event;
+        setNewContent(value);
     };
     return (
         <div>
+            {edit ? (
+                <>
+                    <form onSubmit={onSubmit}>
+                        <input
+                            type='text'
+                            value={newContent}
+                            onChange={onChange}
+                            required
+                        ></input>
+                    </form>
+                    <button
+                        onClick={() => {
+                            setEdit((cur) => !cur);
+                        }}
+                    >
+                        수정
+                    </button>
+                </>
+            ) : null}
             <h4>
                 {'=>'} {tweet.content}
                 {isOwner ? (
                     <button
-                        onClick={() => onEdit(tweet.docId)}
+                        onClick={() => setEdit((cur) => !cur)}
                         style={{ marginLeft: 10 }}
                     >
                         수정
